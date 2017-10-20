9613b8cabefc0eb50a56c0a97d0cb2044e069332
davoigt <daniel.r.voigt@gmx.de>
2017-10-20 14:03:56 +0200

    Update EvaluateCoincidences.cpp

--- EvaluateCoincidences.cpp
+++ EvaluateCoincidences.cpp
@@ -32,25 +32,25 @@ void defineHistogram(TH1* h, std::string x, std::string y, std::string title, bo
 	h->GetXaxis()->SetLabelSize(titleSize);
 	h->GetYaxis()->SetLabelSize(titleSize);
 	h->SetTitleSize(titleSize);
-
+	
 	h->GetXaxis()->SetNdivisions(503, true);
-
+	
 	h->GetYaxis()->SetNdivisions(505, true);
 
 	int lastBin = h->GetNbinsX();
-	if(adjust_range == true)
+	if(adjust_range == true) 
 	{
-		for(int i = 1; i <= h->GetNbinsX(); i++)
+		for(int i = 1; i <= h->GetNbinsX(); i++) 
 		{
 			if(h->GetBinContent(i) > 0)
 				lastBin = i;
 		}
 		lastBin= (int) lastBin * 1.2;
 
-		if(rebin == true)
+		if(rebin == true) 
 		{
 			int bin_factor = (average_entries_per_bin * lastBin / h->GetEntries());
-			if(bin_factor > 1)
+			if(bin_factor > 1) 
 			{
 				h->Rebin(bin_factor);
 				lastBin = lastBin / bin_factor * 1.2;
@@ -79,19 +79,19 @@ void GiveInfo(std::string filename)
     f = new TFile(filename.c_str(), "OPEN");
     t_info = (TTree*) f->Get("InfoTree_layer");
     t_info->SetMakeClass(1);
-
+    
     double serie_start_time;
     double acq_time;
     string chipboard_id;
     double start_time;
     double threshold;
-
+    
     t_info->GetBranch("serie_start_time")->SetAddress(serie_start_time);
     t_info->GetBranch("acq_time")->SetAddress(acq_time);
     t_info->GetBranch("chipboard_id")->SetAddress(chipboard_id);
     t_info->GetBranch("start_time")->SetAddress(start_time);
     t_info->GetBranch("threshold")->SetAddress(threshold);
-
+    
     cout << "Serie Start: " << serie_start_time << endl;
     cout << "Acquisition time: " << acq_time << endl;
     cout << "Chipboard: " << chipboard_id << endl;
@@ -108,7 +108,7 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
 	cout<<"begin program!"<<endl;
 	time_t start_time = time(0);
 	cout<<"maxNumberOfEvents: "<<_maximumNumberOfEntries<<endl;
-
+	
 	gStyle->SetPaintTextFormat("4.1f");
 	cuts = (cuts).c_str();
 
@@ -117,7 +117,7 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
 	set_plot_style();
 
     //int countConversion = 0;
-
+    
 	const int maxNumberOfClusters = 65536;
 	int clstrSize;
 	short PixX[maxNumberOfClusters];
@@ -151,12 +151,12 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
     TH2D* pix_ToA_test = new TH2D("test","",256,-0.5,255.5,256,-0.5,255.5);
     TH2D* pix_ToA_test2 = new TH2D("test2","",256,-0.5,255.5,256,-0.5,255.5);
 
-	int numberOfEvents = 0;
+	int numberOfEvents = 0; 
 	int number_pixels = 0;
 
 	double min_TOA;
 	double max_TOA;
-
+    
     //ofstream strtoagood;
     //strtoagood.open("toagood2.dat");
     //ofstream strtoabad;
@@ -169,14 +169,14 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
     //}
 
 	t_data->Draw(">>myList", cuts.c_str(), "entryList");
-
+    
     cout<<"Starting big loop now!" << endl;
 
 //Daniel: To solve offset in ToA histo, 'dif' wasn't plotted. Instead ToA[j] is now in.
 //Daniel: Redaction: dif is now in again, as well as several testplots, but may be omitted for interest. :P
-
+    
     double ToA_dif_raw[256][256] = { 0 };
-
+    
 	for(unsigned int i = 0; i < myList->GetN(); i++)
 	{
 		if(eventNo_afterCut == -1 || i == eventNo_afterCut)
@@ -193,7 +193,7 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
 				number_pixels++;
 				if(energy_cali == false)
 					pix->Fill(PixX[j],PixY[j],ToT[j]);
-				else{
+				else{ 
 					if(ToT_keV[j] < 0)
 						ToT_keV[j] = 0.1;
 					pix->Fill(PixX[j],PixY[j],ToT_keV[j]);
@@ -202,7 +202,7 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
 				double dif;
                 //if(PixX[j]==193 || PixX[j]==194){ToA[j] = ToA[j] - toacorrection[PixY[j]];}
 				dif = ToA[j] - minToA + 0.00001;
-
+                
                 /*
                 if (PixX[j]==193 || PixX[j]==194) {
                     pix_ToA->Fill(PixX[j], PixY[j], dif-toacorrection[PixY[j]]);
@@ -212,7 +212,7 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
                     pix_ToA->Fill(PixX[j], PixY[j], dif);
                 ToA_dif_raw[PixX[j]][PixY[j]] = ToA_dif_raw[PixX[j]][PixY[j]] + dif;
                // }
-
+				
 				pix_hits->Fill(PixX[j], PixY[j], 1);
                 pix_ToA_test->Fill(PixX[j],PixY[j], ToA[j]);
 			}
@@ -220,15 +220,15 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
 		}
 		if(numberOfEvents >= _maximumNumberOfEntries && _maximumNumberOfEntries > 0){break;}
         //cout << "I'm filling histos. Right now i am at " << i << "from " << myList.GetN() << endl;
-
+        
       /*  if (clstrSize>2 && clstrVolume_keV>750 && clstrVolume_keV < 900) {
             countConversion++;
         }
         */
 	}
-
+    
     cout << "Ending big loop now!" << endl;
-
+    
     for (int k=0; k<256; k++) {
         double bad_bin1 = pix_ToA->GetBinContent(193,k);
         double bad_bin2 = pix_ToA->GetBinContent(194,k);
@@ -239,7 +239,7 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
         //strtoagood << good_bin2 << endl;
         //strtoabad << bad_bin2 << endl;
     }
-
+    
     //To do ToAcorrection in program:
     for (int z=0; z<256 ; z++) {
         double ex3 = ((ToA_dif_raw[192][z] + ToA_dif_raw[193][z])/2) - ((ToA_dif_raw[191][z] + ToA_dif_raw[194][z])/2); // Mittelwert Differenz --> "mittlerer Offset"
@@ -248,20 +248,20 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
         //ToA_dif_raw[192][z]= ToA_dif_raw[191][z];
         //ToA_dif_raw[193][z]= ToA_dif_raw[194][z];
     }
-
+    
     for (int m=0; m<256; m++) {
         for (int n=0; n<256; n++) {
             pix_ToA_test2->Fill(m, n, ToA_dif_raw[m][n]);
         }
     }
     // -----
-
+    
 	TCanvas* c = new TCanvas(("c_"+cuts).c_str(), "result canvas", 900, 1300);
     TCanvas* c_toa = new TCanvas("ToA","control canvas",1300,620);
 	c->Divide(1,2);
     c_toa->Divide(2,1);
 
-	for(short ic = 1; ic <= 2; ic++)
+	for(short ic = 1; ic <= 2; ic++) 
 	{
 		c->GetPad(ic)->SetLeftMargin(0.12);
 		c->GetPad(ic)->SetBottomMargin(0.12);
@@ -290,7 +290,7 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
 	pix->Draw("COLZ9");
 	c->cd(2);
 	pix_ToA_test2->Draw("COLZ9");
-
+    
     c_toa->cd(1);
     pix_Toa_1D_good->Draw();
     c_toa->cd(2);
@@ -308,193 +308,119 @@ void PlotXYC(std::string cuts = "", int eventNo_afterCut = -1, int _maximumNumbe
 
 //Select coincidence group with 2 clusters and look at their energy and time.
 //Daniel: changed cuts to measure conversion electrons in coincidence with K-fluorescences. Plots are not to be trusted now!
-void ScatterPlotEnergiesCoincidenceGroups(std::string filename){
-    int countConversion =0;
-	f = new TFile(filename.c_str(), "OPEN");
-	TTree* t = (TTree*) f->Get("clusteredData");
-
-	set_plot_style();
-
-	float clstrVolume_keV;
-	long coinc_group_no;
-	int clstrSize;
-	double clstrTime;
-
-	//load the needed branches;
-	t->GetBranch("clstrVolume_keV")->SetAddress(&clstrVolume_keV); //energy per cluster
-	t->GetBranch("coincidence_group")->SetAddress(&coinc_group_no);
-	t->GetBranch("clstrSize")->SetAddress(&clstrSize);
-	t->GetBranch("min_ToA")->SetAddress(&clstrTime);
-
-	//Create and entry list of interesting events;
-	t->Draw(">>myList", "coincidence_group_size == 2", "entryList");
-
-	//containers for the coincidence group results
-	double energies[2] = {0};
-	double time[2] = {0};
-	int size[2] = {0};
-
-	//Define the histrograms
-	TH1F* h_dt = new TH1F("h_dt", "", 100, 0, 156);
-	TH1F* h_e = new TH1F("h_e", "", 1500, 0, 1500);
-	//scatter plot of energies of event 1 and event2
-	TH2F* h_e1e2 = new TH2F("h_e1e2", "", 1000, 0, 1000, 1000, 0, 1000);
-
-	long coinc_group_no_temp = -1;
-	//int maxEvents = 10000;
-
-	int j = 0;
-	for(long i = 0; i < myList->GetN(); i++){
-		//Go through the entry list and do some analysis;
-		t->GetEntry(myList->GetEntry(i));
-
-		if(coinc_group_no == coinc_group_no_temp){
-			size[1] = clstrSize;
-			time[1] = clstrTime;
-			energies[1] = clstrVolume_keV;
-
-//Daniel: HIER DIE CUTS FUER COINCIDENCES WOHOOO!!! :-< :-< :-< :-< :-< :-< :-< :-< :-< <--DAS SIND SCHEREN SCHNIP SCHNIP
-
-/*
-			if(size[0] <= 2 && size[1] <= 2 && energies[0] + energies[1] < 18){
-				h_e1e2->Fill(energies[0], energies[1]);
-				h_dt->Fill(fabs(time[1] - time[0]));
-				h_e->Fill(energies[0] + energies[1]);
-                countConversion++;
-			}
- */
-            if(size[0] == 2 && size[1] > 2 && energies[0] <20 && energies[1] > 750 && energies[1] < 900){
-                h_e1e2->Fill(energies[0], energies[1]);
-                h_dt->Fill(fabs(time[1] - time[0]));
-                h_e->Fill(energies[0] + energies[1]);
-                countConversion++;
-            }
-		}
-		else{
-			energies[0] = clstrVolume_keV;
-			size[0] = clstrSize;
-			time[0] = clstrTime;
-			coinc_group_no_temp = coinc_group_no;
-		}
-	}
-
-	//Define Canvas and plot the results
-	TCanvas** c = new TCanvas*[3];
-	for(int j = 0; j < 3; j++){
-		c[j] = new TCanvas();
-		c[j]->SetBottomMargin(0.11);
-		c[j]->SetLeftMargin(0.11);
-	}
-
-	c[0]->cd();
-	defineHistogram(h_e1e2, "Energy [keV]", "Energy [keV]", "Coinc. Group Size: 2, cluster size <= 2", false);
-	h_e1e2->GetXaxis()->SetTitleOffset(0.9);
-	h_e1e2->GetYaxis()->SetTitleOffset(0.9);
-	h_e1e2->Draw("COLZ");
-
-	c[1]->cd();
-	defineHistogram(h_dt, "#Deltat (ns)", "Number of events", "Coinc. Group Size: 2, cluster size <= 2", false);
-	h_dt->GetXaxis()->SetTitleOffset(0.9);
-	h_dt->GetYaxis()->SetTitleOffset(0.9);
-	h_dt->Draw("");
-
-	c[2]->cd();
-	defineHistogram(h_e, "#SigmaE (keV)", "Number of events", "Coinc. Group Size: 2, cluster size <= 2", false);
-	h_e->GetXaxis()->SetTitleOffset(0.9);
-	h_e->GetYaxis()->SetTitleOffset(0.9);
-	h_e->Draw("");
-    cout << "Found " << countConversion << " conversion electrons in coincidence with fluorescences!" << endl;
-}
-
 void ScatterPlotEnergies3CoincidenceGroups(std::string filename){
     int countConversion =0;
-	f = new TFile(filename.c_str(), "OPEN");
-	TTree* t = (TTree*) f->Get("clusteredData");
-
-	set_plot_style();
-
-	float clstrVolume_keV;
-	long coinc_group_no;
-	int clstrSize;
-	double clstrTime;
-	long coinc_group_size;
-
-	//load the needed branches;
-	t->GetBranch("clstrVolume_keV")->SetAddress(&clstrVolume_keV); //energy per cluster
-	t->GetBranch("coincidence_group")->SetAddress(&coinc_group_no);
-	t->GetBranch("coincidence_group_size")->SetAddress(&coinc_group_size);
-	t->GetBranch("clstrSize")->SetAddress(&clstrSize);
-	t->GetBranch("min_ToA")->SetAddress(&clstrTime);
-
-	//Create and entry list of interesting events;
-	t->Draw(">>myList", "coincidence_group_size == 3", "entryList");
-
-	//containers for the coincidence group results
-	double energies[3] = {0};
-	double time[3] = {0};
-	int size[3] = {0};
-
-	//Define the histrograms
-	TH1F* h_dt = new TH1F("h_dt", "", 100, 0, 156);
-	TH1F* h_e = new TH1F("h_e", "", 1500, 0, 1500);
-	//scatter plot of energies of event 1 and event2
-	//TH2F* h_e1e2 = new TH2F("h_e1e2", "", 1000, 0, 1000, 1000, 0, 1000);
-
-	long coinc_group_no_temp = -1;
-	//int maxEvents = 10000;
-
-	int j = 0;
-	for(long i = 0; i < myList->GetN(); i++){
-		//Go through the entry list and do some analysis;
-		t->GetEntry(myList->GetEntry(i));
-    if(coinc_group_size==3)
-    {
-        for(long k = 0; k < 3; k++)
+    f = new TFile(filename.c_str(), "OPEN");
+    TTree* t = (TTree*) f->Get("clusteredData");
+    
+    set_plot_style();
+    
+    float clstrVolume_keV;
+    long coinc_group_no;
+    int clstrSize;
+    double clstrTime;
+    long coinc_group_size;
+    short PixX[65536];
+    short PixY[65536];
+    float ToT_keV[65536];
+    
+    //load the needed branches;
+    t->GetBranch("clstrVolume_keV")->SetAddress(&clstrVolume_keV); //energy per cluster
+    t->GetBranch("coincidence_group")->SetAddress(&coinc_group_no);
+    t->GetBranch("coincidence_group_size")->SetAddress(&coinc_group_size);
+    t->GetBranch("clstrSize")->SetAddress(&clstrSize);
+    t->GetBranch("min_ToA")->SetAddress(&clstrTime);
+    t->GetBranch("ToT_keV")->SetAddress(ToT_keV);
+    t->GetBranch("PixX")->SetAddress(PixX);
+    t->GetBranch("PixY")->SetAddress(PixY);
+    
+    
+    //Create and entry list of interesting events;
+    t->Draw(">>myList", "coincidence_group_size == 3", "entryList");
+    
+    //containers for the coincidence group results
+    double energies[3] = {0};
+    double time[3] = {0};
+    int size[3] = {0};
+    
+    //Define the histrograms
+    TH1F* h_dt = new TH1F("h_dt", "", 100, 0, 156);
+    TH1F* h_e = new TH1F("h_e", "", 1000, 0, 1000);
+    TH2F* pix = new TH2F("pixelmatrix", "", 256, -0.5,255.5,256,-0.5,255.5);
+    //scatter plot of energies of event 1 and event2
+    //TH2F* h_e1e2 = new TH2F("h_e1e2", "", 1000, 0, 1000, 1000, 0, 1000);
+    
+    long coinc_group_no_temp = -1;
+    //int maxEvents = 10000;
+    
+    int j = 0;
+    for(long i = 0; i < myList->GetN(); i++){
+        //Go through the entry list and do some analysis;
+        t->GetEntry(myList->GetEntry(i));
+        if(coinc_group_size==3)
         {
-            t -> GetEntry(myList->GetEntry(i+k));
-            energies[k] = clstrVolume_keV;
-			size[k] = clstrSize;
-			time[k] = clstrTime;
-        }
-//Daniel: HIER DIE CUTS FUER COINCIDENCES WOHOOO!!! :-< :-< :-< :-< :-< :-< :-< :-< :-< <--DAS SIND SCHEREN SCHNIP SCHNIP
-
-/*
-			if(size[0] <= 2 && size[1] <= 2 && energies[0] + energies[1] < 18){
-				h_e1e2->Fill(energies[0], energies[1]);
-				h_dt->Fill(fabs(time[1] - time[0]));
-				h_e->Fill(energies[0] + energies[1]);
-                countConversion++;
-			}
- */
-            if(size[0] == 1 && size[1] == 1 && size[2] > 2 && energies[0] <10  && energies[1] < 10 && energies[2] > 750 && energies[2] < 900){
-               // h_e1e2->Fill(energies[0], energies[1]);
+            for(long k = 0; k < 3; k++)
+            {
+                t -> GetEntry(myList->GetEntry(i+k));
+                energies[k] = clstrVolume_keV;
+                size[k] = clstrSize;
+                time[k] = clstrTime;
+            }
+            //Daniel: HIER DIE CUTS FUER COINCIDENCES WOHOOO!!! :-< :-< :-< :-< :-< :-< :-< :-< :-< <--DAS SIND SCHEREN SCHNIP SCHNIP
+            
+            /*
+             if(size[0] <= 2 && size[1] <= 2 && energies[0] + energies[1] < 18){
+             h_e1e2->Fill(energies[0], energies[1]);
+             h_dt->Fill(fabs(time[1] - time[0]));
+             h_e->Fill(energies[0] + energies[1]);
+             countConversion++;
+             }
+             */
+            if((size[0] == 1 && size[1] == 1 && size[2] > 2 && energies[0] <10  && energies[1] < 10 && energies[2] > 750 && energies[2] < 900) || (size[1] == 1 && size[2] == 1 && size[0] > 2 && energies[1] <10  && energies[2] < 10 && energies[0] > 750 && energies[0] < 900) || (size[2] == 1 && size[0] == 1 && size[1] > 2 && energies[2] <10  && energies[0] < 10 && energies[1] > 750 && energies[1] < 900) ){
+                // h_e1e2->Fill(energies[0], energies[1]);
                 h_dt->Fill(fabs(time[2] - time[0]));
-                h_e->Fill(energies[2]);
+                for (int m = 0; m < 3; m++) {
+                    if (energies[m]>600) {
+                        h_e->Fill(energies[m]);
+                        for (int n=0; n < size[m]; n++) {
+                            pix->Fill(PixX[n],PixY[n],ToT_keV[n]);
+                       }
+                    }
+                }
+                
+                
                 countConversion++;
             }
-		}
-	}
-	}
-	//Define Canvas and plot the results
-	TCanvas** c = new TCanvas*[2];
-	for(int j = 0; j < 2; j++){
-		c[j] = new TCanvas();
-		c[j]->SetBottomMargin(0.11);
-		c[j]->SetLeftMargin(0.11);
-	}
+        }
+    }
 
-	c[0]->cd();
-	defineHistogram(h_dt, "#Deltat (ns)", "Number of events", "Coinc. Group Size: 3, cluster size <= 2", false);
-	h_dt->GetXaxis()->SetTitleOffset(0.9);
-	h_dt->GetYaxis()->SetTitleOffset(0.9);
-	h_dt->Draw("");
+//Define Canvas and plot the results
+TCanvas** c = new TCanvas*[2];
+for(int j = 0; j < 3; j++){
+    c[j] = new TCanvas();
+    c[j]->SetBottomMargin(0.11);
+    c[j]->SetLeftMargin(0.11);
+}
 
-	c[1]->cd();
-	defineHistogram(h_e, "#SigmaE (keV)", "Number of events", "Coinc. Group Size: 3, cluster size <= 2", false);
-	h_e->GetXaxis()->SetTitleOffset(0.9);
-	h_e->GetYaxis()->SetTitleOffset(0.9);
-	h_e->Draw("");
-    cout << "Found " << countConversion << " conversion electrons in coincidence with fluorescences!" << endl;
+c[0]->cd();
+defineHistogram(h_dt, "#Deltat (ns)", "Number of events", "Coinc. Group Size: 3, cluster size <= 2", false);
+h_dt->GetXaxis()->SetTitleOffset(0.9);
+h_dt->GetYaxis()->SetTitleOffset(0.9);
+h_dt->Draw("");
+
+c[1]->cd();
+defineHistogram(h_e, "#SigmaE (keV)", "Number of events", "Coinc. Group Size: 3, cluster size <= 2", false);
+h_e->GetXaxis()->SetTitleOffset(0.9);
+h_e->GetYaxis()->SetTitleOffset(0.9);
+h_e->Draw("");
+
+c[2]->cd();
+defineHistogram(pix, "#ToT (keV)", "Number of events", "Coinc. Group Size: 3, cluster size > 2", false);
+pix->GetXaxis()->SetTitleOffset(0.9);
+pix->GetYaxis()->SetTitleOffset(0.9);
+pix->Draw("COLZ9");
+
+cout << "Found " << countConversion << " conversion electrons in coincidence with fluorescences!" << endl;
 }
 
 
@@ -530,7 +456,7 @@ void SaveEnergySpectrum(int run_start, int run_end, std::string out = ""){
 	c->SetLogy();
 	c->SetLeftMargin(0.12);
 	c->SetBottomMargin(0.12);
-
+	
 	TH1F* h_run;
 
 	for(int i = run_start; i <= run_end; i++){
@@ -543,7 +469,7 @@ void SaveEnergySpectrum(int run_start, int run_end, std::string out = ""){
 		h_run->Clear();
 	}
 	defineHistogram(h_tot, "Cluster volume (keV)", "No. of events", "Mn-54: 10 h", false);
-
+	
 
 	h_tot->Draw();
 	if(out.compare("") != 0){
@@ -560,18 +486,18 @@ void SaveEnergySpectrum(int run_start, int run_end, std::string out = ""){
 }
 
 //Daniel: Gives possibility to integrate over x-Values in total energy spectrum
-
+/*
 void IntegralEnergy(int xmin, int xmax)
 {
-
+    
     TAxis *axis = h_tot->GetXaxis();
     int bmin = axis->FindBin(xmin);
     int bmax = axis->FindBin(xmax);
     double integral = h_tot->Integral(bmin,bmax);
     cout << integral << endl;
-
+    
 }
-
+*/
 //Daniel: RETRACTEDRETRACTEDRETRACTEDRETRACTED
 //Daniel: The SaveEnergySpectrum-function can sum up all runXX.root Files and evaluate them.
 // In the following section, the same tactic as used in this function will be used for PlotXYC with different, potentially interesting Cuts.
@@ -583,9 +509,9 @@ void PlotSmallCoincidences(int run_start, int run_end, std::string out = "")
     c->SetLogy();
     c->SetLeftMargin(0.12);
     c->SetBottomMargin(0.12);
-
+    
     TH1F* h_run;
-
+    
     for(int i = run_start; i <= run_end; i++){
         //TH1F* h = new TH1F("h" , "", 1000, 0, 2000);
         std::stringstream filename;
@@ -596,8 +522,8 @@ void PlotSmallCoincidences(int run_start, int run_end, std::string out = "")
         h_run->Clear();
     }
     defineHistogram(h_tot, "Cluster volume (keV)", "No. of events", "Mn-54: 10 h", false);
-
-
+    
+    
     h_tot->Draw();
     if(out.compare("") != 0){
         c->Print( (out+".pdf").c_str(), "pdf");
